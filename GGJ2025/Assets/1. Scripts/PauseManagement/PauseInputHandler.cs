4ab0b2644d452aa987075ad38bb4cc131004f393
Danny-vD <dvduist@hotmail.nl>
2025-01-24 21:01:52 +0100

    Handle input for the pause

--- GGJ2025/Assets/1. Scripts/PauseManagement/PauseInputHandler.cs
+++ GGJ2025/Assets/1. Scripts/PauseManagement/PauseInputHandler.cs
@@ -14,5 +14,34 @@ namespace PauseManagement
 
 		[SerializeField]
 		private InputActionReference togglePauseInput;
+
+		private void OnEnable()
+		{
+			pauseInput.action.performed       += Pause;
+			resumeInput.action.performed      += Resume;
+			togglePauseInput.action.performed += TogglePause;
+		}
+
+		private void OnDisable()
+		{
+			pauseInput.action.performed       -= Pause;
+			resumeInput.action.performed      -= Resume;
+			togglePauseInput.action.performed -= TogglePause;
+		}
+
+		public void Pause(InputAction.CallbackContext callbackContext)
+		{
+			PauseManager.Pause();
+		}
+
+		public void Resume(InputAction.CallbackContext callbackContext)
+		{
+			PauseManager.Resume();
+		}
+
+		public void TogglePause(InputAction.CallbackContext callbackContext)
+		{
+			PauseManager.TogglePause();
+		}
 	}
 }
