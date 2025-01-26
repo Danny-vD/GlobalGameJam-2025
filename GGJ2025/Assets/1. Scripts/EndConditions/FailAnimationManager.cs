57948059d83f851156909c583c3e85ce8b340efc
Danny-vD <dvduist@hotmail.nl>
2025-01-26 10:15:52 +0100

    Play animations and prevent movement on a fail condition

---
+++ GGJ2025/Assets/1. Scripts/EndConditions/FailAnimationManager.cs
@@ -0,0 +1,47 @@
+﻿using GameplayEvents;
+using GameplayEvents.Enums;
+using SerializableDictionaryPackage.SerializableDictionary;
+using UnityEngine;
+using VDFramework;
+using VDFramework.EventSystem;
+
+namespace EndConditions
+{
+	[RequireComponent(typeof(Animator))]
+	public class FailAnimationManager : BetterMonoBehaviour
+	{
+		[SerializeField]
+		private SerializableEnumDictionary<CauseOfFailure, string> triggerPerFailCondition;
+
+		private Animator animator;
+
+		private void Awake()
+		{
+			animator = GetComponent<Animator>();
+		}
+
+		private void OnEnable()
+		{
+			EventManager.AddListener<PlayerFailedEvent>(PlayAnimation);
+		}
+
+		private void OnDisable()
+		{
+			EventManager.RemoveListener<PlayerFailedEvent>(PlayAnimation);
+		}
+
+		private void PlayAnimation(PlayerFailedEvent playerFailedEvent)
+		{
+			CauseOfFailure causeOfFailure = playerFailedEvent.CauseOfFailure;
+
+			if (causeOfFailure == CauseOfFailure.None)
+			{
+				return;
+			}
+			
+			string trigger = triggerPerFailCondition[causeOfFailure];
+			
+			animator.SetTrigger(trigger);
+		}
+	}
+}
