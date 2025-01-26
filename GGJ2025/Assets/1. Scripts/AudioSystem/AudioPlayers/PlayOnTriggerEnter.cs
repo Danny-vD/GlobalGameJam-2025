6a5a393f04f517a28d6991ecc8ab3dc96b2893a0
Danny-vD <dvduist@hotmail.nl>
2025-01-26 05:50:05 +0100

    Add an audio player for when you enter a trigger

---
+++ GGJ2025/Assets/1. Scripts/AudioSystem/AudioPlayers/PlayOnTriggerEnter.cs
@@ -0,0 +1,23 @@
+﻿using AudioSystem.Enums;
+using UnityEngine;
+using VDFramework;
+
+namespace AudioSystem.AudioPlayers
+{
+	public class PlayOnTriggerEnter : BetterMonoBehaviour
+	{
+		[SerializeField]
+		private AudioEvent audioEvent;
+
+		private void OnTriggerEnter(Collider other)
+		{
+			PlayAudio();
+		}
+
+		private void PlayAudio()
+		{
+			AudioPlayer.PlayAudio(audioEvent);
+		}
+
+	}
+}
