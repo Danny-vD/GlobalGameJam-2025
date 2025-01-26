cce5a818eee1ff0646a9a5670133b3d126aa2ef3
Danny-vD <dvduist@hotmail.nl>
2025-01-26 11:34:04 +0100

    Let the controller know the dodge is complete

---
+++ GGJ2025/Assets/1. Scripts/PlayerControls/PlayerAnimationEvents.cs
@@ -0,0 +1,20 @@
+﻿using CMF;
+using VDFramework;
+
+namespace PlayerControls
+{
+	public class PlayerAnimationEvents : BetterMonoBehaviour
+	{
+		private Controller controller;
+		
+		private void Awake()
+		{
+			controller = GetComponentInParent<Controller>();
+		}
+
+		public void DodgeEnded()
+		{
+			controller.DodgeEnded();
+		}
+	}
+}
